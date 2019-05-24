2b2f8c56ee7c0bede4ebc62de24c6e5adc32b7fc
Ken Lomax <ken.lomax@sap.com>
2019-05-24 15:32:39 +0200

    json

--- gcfexample/index.js
+++ gcfexample/index.js
@@ -1,21 +1,9 @@
-// For debugging support
+// For stackdriver support
 const debug = require ('@google-cloud/debug-agent').start({allowExpressions:true});
 // End
 
-initjson = (json) => {
-  // when called via "functions call", the json.daa is found in body..
-  if (json && json.body)
-    json = json.body
-  // When called via events (see later), json is in json.data..
-  if (json && json.data)
-    json = json.data;
-  return json
-}
-  
-  
 exports.helloWorld = (json, res) => {
-  json= initjson(json)
-  console.log("Hello "+ json.name)
+  json= findJson(json)
   if (res && res.send)
     res.status(200).send("Hello "+ json.name);
 }
@@ -27,114 +15,116 @@ var speechModule = require('@google-cloud/speech');
 var translateModule = require('@google-cloud/translate');
 var ttsModule = require('@google-cloud/text-to-speech');
    
-// Client side APIs
+// Clients
 var storage = new storageModule.Storage();
 var vision = new visionModule.ImageAnnotatorClient();
 var speech = new speechModule.SpeechClient();
 var translate = new translateModule.Translate()
 var textToSpeech = new ttsModule.TextToSpeechClient();
-   
-// In-built util module
-var util = require("util")
-  
-// A useful logging function
-track = (ID, input, step) => {
-    console.log(  `ID ${ID} Step ${step} : ${JSON.stringify(input)}` )
-}
-   
+
+var id = 0 ;
+
 exports.gcfs = (json, res) => {
-    json = initjson(json)    
-    var sourceFile = "gs://" + json.bucket + "/" + json.name 
-    console.log(`Function gcfs: ${sourceFile}`)
-    var ID = new Date().getTime();
-    var step = 1
-   
-    // Deal with files whose name starts with "input", otherwise return nicely.
-    if(!json.name.startsWith("input") && !json.name.contains("/input")){
-        if (res && res.status) // When called via functions called, we have a res.
-                res.status(200).send("SUCCESS");
-            else if (res) // when called via event, we should call the callback
-                res();
-            return;
-    }
-  
-    storage.bucket(json.bucket).file(json.name).download(). // 0
-        then((input) => { // 1 
-            // Speech Recognition 1a
-            if (json.name.endsWith(".wav")){
-                track( ID, input[0].length, step++ )
-                return speech.recognize( {config: { languageCode: 'en-US' },audio: { uri: sourceFile }})
+    
+    id++;
+    var step = 0
+
+    json = findJson(json)    
+    storage.bucket(json.bucketin).file(json.file).download(). 
+    then((input) => { 
+        logging( id, input, step++ )
+        var sourceFile = "gs://" + json.bucketin + "/" + json.file 
+        // Speech recognition
+        if (json.file.endsWith(".wav"))
+            return speech.recognize( {config: { languageCode: json.sourceLocale  ? json.sourceLocale  : "en-US" }, audio: {content:  input[0] } }  );   
+        // Text Recognition     
+        if (json.file.endsWith(".jpeg"))
+            return vision.textDetection( sourceFile );   
+        // Text file    
+        return input;
+    }).
+    then((input) => { 
+        // Pull the recognized text from the json
+        logging( id, input, step++ );
+        if ( input[0].fullTextAnnotation )
+            return input[0].fullTextAnnotation.text.replace(/\n/g, " ").trim();
+        else if ( input[0].results )
+            return input[0].results[0].alternatives[0].transcript;
+        else // txt file
+            return input[0];
+    }).
+    then((input) => {  
+        // Modify the text (add some manners)
+        logging( id, input, step++ )         
+        var tips =[
+            "Sorry to bother.",
+            "Would you mind.",
+            "Forgive the intrusion.",
+            "Thank you so much.",
+            "You are too kind.",
+            "One is most grateful."
+        ];        
+        var tip = tips[Math.floor(Math.random() * tips.length)]       
+        return tip+" "+input;
+    }).
+    then((input) => {  
+        // Translate to another language         
+        logging( id, input, step++ )
+        const options = {
+            from: "en",
+            to: json.targetLocale  ? json.targetLocale  : "de"
+        };
+        return translate.translate(input, options);
+    }).
+    then((input) => {    
+        // Text to speech to synthesize WAV file      
+        logging( id, input, step++ )
+        const ttsParams = {
+            input: {
+                text: input[0]
+            },
+            voice: {
+                languageCode: json.targetLocale  ? json.targetLocale  : "de",
+                ssmlGender: json.gender  ? json.gender  : 'FEMALE'
+            },
+            audioConfig: {
+                audioEncoding: 'LINEAR16'
             }
-            // Optical Character Recognition 1b
-            if (json.name.endsWith(".jpeg")){
-                track( ID, input[0].length, step++ )
-                return vision.textDetection(sourceFile)
-            }
-            track( ID, input, step++ )
-            return input
-        }).
-        then((input) => { // 2
-            // Pull the recognized text from the json
-            track( ID, input, step++ )
-            if (input[0].fullTextAnnotation)
-                return input[0].fullTextAnnotation.text.replace(/\n/g, " ").trim();
-            else if ( input[0].results)
-                return input[0].results[0].alternatives[0].transcript;
-            else // txt file
-                return input[0]
-        }).
-        then((input) => { // 3   
-            // Modify the text (add some manners)
-            track( ID, input, step++ )         
-            var tips =["Sorry to bother.",
-                "Would you mind.",
-                "Forgive the intrusion.",
-                "Thank you so much.",
-                "You are too kind.",
-                "One is most grateful."]        
-            var tip = tips[Math.floor(Math.random() * tips.length)]       
-            return tip+" "+input;
-        }).
-        then((input) => { // 4  
-            // Translate to another language         
-            track( ID, input, step++ )
-            const options = {
-                from: "en",
-                to: json.targetLocale  ? json.targetLocale  : "de"
-            };
-            return translate.translate(input, options);
-        }).
-        then((input) => { // 5     
-            // Text to speech to synthesize WAV file      
-            track( ID, input, step++ )
-            const ttsParams = {
-                input: {
-                    text: input[0]
-                },
-                voice: {
-                    languageCode: json.targetLocale  ? json.targetLocale  : "de",
-                    ssmlGender: 'FEMALE'
-                },
-                audioConfig: {
-                    audioEncoding: 'LINEAR16'
-                }
-            }
-            return textToSpeech.synthesizeSpeech(ttsParams);
-        }).
-        then((input) => { // 6
-            // Save to the bucket
-            track( ID, input[0].audioContent.length, step++ ) 
-            return storage.bucket(json.bucket).file("AUDIO_"+json.name+".wav").save(input[0].audioContent);
-        }).
-        then((input) => { // 7
-            // Return cleanly
-            track( ID, input, step++ )
-            if (res && res.status) // When called via functions called, we have a res.
-                res.status(200).send("SUCCESS");
-            else if (res)
-                res(); // when called via event, we should call the callback
-            return;
-        })
+        }
+        return textToSpeech.synthesizeSpeech(ttsParams);
+    }).
+    then((input) => { 
+        // Save to the bucket
+        logging( id, input[0].audioContent.length, step++ ) 
+        return storage.bucket(json.bucketout).file("AUDIO_"+json.file+".wav").save(input[0].audioContent);
+    }).
+    then((input) => { 
+        // Return cleanly
+        logging( id, input, step++ )
+        if (res && res.status) // if http call, we should return 200
+            res.status(200).send("SUCCESS");
+        return;
+    })
 }
 
-// For debugging support (see https://medium.com/google-cloud/stackdriver-debugger-on-google-cloud-functions-d49a6ee5f12c)
+
+findJson = (json) => {
+ if (json && json.body) // when called via google Testing tab
+    json = json.body  
+  else if (json && json.query) // when called via curl, json is in query
+    json = json.query  
+  else  {
+    // When called via events, we need to juggle the json
+    json.bucketin=json.bucket;
+    json.file=json.name;
+    json.bucketout="gcfoutput"
+  }
+  
+  console.log("findJson: "+ JSON.stringify(json))
+
+  return json
+}
+  
+logging = (id, input, step) => {
+    console.log(  `id ${id} Step ${step} : ${JSON.stringify(input)}` )
+}
