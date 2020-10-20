5fda9b14184dc82cc4119dfdccb51e6ce0ace474
kenlomaxhybris <ken.lomax@sap.com>
2020-10-20 15:30:17 +0200

    Update index.js
    exports.helloWorld

--- node101/index.js
+++ node101/index.js
@@ -1,16 +1,15 @@
 var expressModule = require('express')
 var expressWebServer = expressModule();
 
-expressWebServer.get('/', function(req, res){ 
-  helloWorld(req,res);
-});
-
-helloWorld = (json, res) => {
+exports.helloWorld = (json, res) => {
   console.log("Hello "+ json.query.name)
   if (res && res.send)
     res.status(200).send("Hello "+ json.query.name);
 }
-expressWebServer.listen(8080);
+
+expressWebServer.get('/', function(req, res){ 
+  helloWorld(req,res);
+});
 
 console.log("Server is up and running.");
 console.log('curl "http://localhost:8080?name=TheresaMay"');
