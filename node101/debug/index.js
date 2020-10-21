7f463c52c87a7f1c06b30ee380a8f92e28cf6d80
kenlomaxhybris <ken.lomax@hybris.com>
2020-10-21 07:24:45 +0000

    Added debug file

---
+++ node101/debug/index.js
@@ -0,0 +1,16 @@
+var expressModule = require('express')
+var expressWebServer = expressModule();
+const debug = require ('@google-cloud/debug-agent').start({allowExpressions:true});
+
+exports.helloWorld = (json, res) => {
+  console.log("Hello "+ json.query.name)
+  if (res && res.send)
+    res.status(200).send("Hello "+ json.query.name);
+}
+
+expressWebServer.get('/', function(req, res){ 
+  helloWorld(req,res);
+});
+
+console.log("Server is up and running.");
+console.log('curl "http://localhost:8080?name=TheresaMay"');
